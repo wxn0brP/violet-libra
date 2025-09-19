3b01670cf48a8f50b22cb62e37cbf4b6a233d5ca
wxn0brP <toja.filiper@gmail.com>
2025-09-19 19:25:07 +0200

    feat: test resolver

--- back/api/resolver.ts
+++ back/api/resolver.ts
@@ -2,3 +2,12 @@ import { PermissionResolverEngine } from "@wxn0brp/vql/permissions/resolver";
 
 export const resolver = new PermissionResolverEngine();
 
+resolver.addResolver(
+    async (_, p) => {
+        return p[0] === "api";
+    },
+    async (args) => {
+        return true;
+    }
+);
+
