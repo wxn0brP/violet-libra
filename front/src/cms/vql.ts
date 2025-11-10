8d9741bd1f5c9a1c351c03eda15d16fd6e48f350
wxn0brP <toja.filiper@gmail.com>
2025-11-10 23:46:44 +0100

    feat: /cms probably logout

--- front/src/cms/vql.ts
+++ front/src/cms/vql.ts
@@ -1,12 +1,15 @@
+import { confirm } from "@wxn0brp/flanker-dialog";
 import { fetchVQL, VConfig } from "@wxn0brp/vql-client";
 
 VConfig.url = `/VQL?token=${localStorage.getItem("authToken")}`;
-VConfig.hooks.onError = (query, e, res) => {
+VConfig.hooks.onError = async (query, e, res) => {
     console.log("onError");
     console.log("Q", query);
     console.log("E", e);
     console.log("R", res);
     if (!res || res.c === 403) {
+        const conf = await confirm("Your session probably has expired. Do you want to log out?");
+        if (!conf) return;
         localStorage.removeItem("authToken");
         localStorage.removeItem("userName");
         window.location.href = "/login?next=cms";
