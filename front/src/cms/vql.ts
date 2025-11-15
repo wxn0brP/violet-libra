352dde6dc341fd28a6a4da223f49c992ad186f18
wxn0brP <toja.filiper@gmail.com>
2025-11-16 00:48:56 +0100

    feat: improve /cms session expired

--- front/src/cms/vql.ts
+++ front/src/cms/vql.ts
@@ -1,6 +1,15 @@
 import { confirm } from "@wxn0brp/flanker-dialog";
 import { fetchVQL, VConfig } from "@wxn0brp/vql-client";
 
+function off() {
+    localStorage.removeItem("authToken");
+    localStorage.removeItem("userName");
+    window.location.href = "/login?next=cms";
+}
+
+if (!localStorage.getItem("authToken")) off();
+
+let sessionQuestion = 0;
 VConfig.url = `/VQL?token=${localStorage.getItem("authToken")}`;
 VConfig.hooks.onError = async (query, e, res) => {
     console.log("onError");
@@ -8,12 +17,18 @@ VConfig.hooks.onError = async (query, e, res) => {
     console.log("E", e);
     console.log("R", res);
     if (!res || res.c === 403) {
+        // Do not spam
+        if (Date.now() - sessionQuestion > 10_000)
+            return;
+
         const conf = await confirm("Your session probably has expired. Do you want to log out?");
-        if (!conf) return;
-        localStorage.removeItem("authToken");
-        localStorage.removeItem("userName");
-        window.location.href = "/login?next=cms";
+        if (!conf) {
+            sessionQuestion = Date.now();
+            return;
+        }
+
+        off();
     }
 }
 
-fetchVQL("api-cms-admin md s.id=0").catch(() => { });
+fetchVQL("api-cms-admin md s.id=0").catch(() => off());
